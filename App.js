29a53bb3f2350c0009bc9e3c5ab309e391167d34
Benjamin <benjamindeltour22@gmail.com>
2023-09-20 11:55:20 +0200

    upgrade bottom navbar

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AccueilScreen } from './views/AccueilScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { LoginScreen } from './views/LoginScreen';
+import { Icon } from '@rneui/themed';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,7 +10,22 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator screenOptions={({route})=>({ 
+        headerShown: false,
+        tabBarIcon: () => {
+
+          let iconName
+
+          if (route.name == "Accueil") {
+              iconName = "home"
+          }
+          if (route.name == "Login") {
+              iconName = "user"
+          }
+
+          return <Icon name={iconName} type='font-awesome' size={25} color={'black'} />
+      }
+      })}>
         <Tab.Screen name="Accueil" component={AccueilScreen} />
         <Tab.Screen name="Login" component={LoginScreen} />
       </Tab.Navigator>
